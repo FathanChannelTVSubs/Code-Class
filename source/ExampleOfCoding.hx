2111e60b2b8be47c6c2cd3309d97eddc13767b1a
FathanChannelTVSubs <96095677+FathanChannelTVSubs@users.noreply.github.com>
2022-01-20 12:13:20 +0700

    Create ExampleOfCoding.hx

---
+++ source/ExampleOfCoding.hx
@@ -0,0 +1,55 @@
+IMPORTS:
+import flixel.Fathan;
+import flixel.Haxe;
+import flixel.Class;
+import flixel.Code;
+import flixel.Github;
+import flixel.NG;
+
+TO START CODING:
+{
+  
+}
+
+TRUE AND FALSE:
+=true
+=false
+  
+BLOCKED CODE:
+// SUBSCRIBE PLS //
+  
+INSIDE:
+(Hahahaha)
+[Hahaha]
+{Hahahahaha}
+<LOL>
+
+USING:
+using Blablabla;
+
+CLASS:
+class ExampleOfCoding;
+
+PUBLIC STATIC VAR:
+public static var
+
+ARRAY:
+Array<Fathan>
+  
+PUBLIC STATIC FUNCTION:
+public static function
+
+RETURN:
+return POPit
+
+MINUS:
+-1
+  
+NUMBERS:
+1234567890
+  
+NULL:
+null oofUp
+  
+OVERRIDE FUCTION:
+override function
